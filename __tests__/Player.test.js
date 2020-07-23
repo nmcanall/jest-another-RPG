9398ff034589473321df1a10268501918bb3dc96
nmcanall <nmcanall@citadel.edu>
2020-07-22 21:10:25 -0700

    Player's getHealth, isAlive, and reduceHealth methods

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -33,4 +33,31 @@ test("gets inventory from player or returns false", () => {
     player.inventory = [];
     
     expect(player.getInventory()).toEqual(false);
-})
+});
+
+test("gets player's health", () => {
+    const player = new Player("Nathan");
+
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+
+test("tests if player is alive or not", () => {
+    const player = new Player("Nathan");
+
+    expect(player.isAlive()).toBeTruthy();
+
+    player.health = 0;
+    expect(player.isAlive()).toBeFalsy();
+});
+
+test("tests if player's health was reduced", () => {
+    const player = new Player("Nathan");
+
+    const oldHealth = player.health;
+
+    player.reduceHealth(5);
+    expect(player.health).toBe(oldHealth - 5);
+
+    player.reduceHealth(999999);
+    expect(player.health).toBe(0);
+});
